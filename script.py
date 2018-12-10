386d64c492331fe6aa85b86eefa3e47a2509e1dc
Meghna Mathur <43182176+Megmath@users.noreply.github.com>
2018-12-09 23:05:13 -0600

    Update script.py

--- script.py
+++ script.py
@@ -1,169 +1,133 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
-"""
-
-
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import wordcloud
+import matplotlib.pyplot as plot
 from wordcloud import WordCloud, STOPWORDS
-# Reading ratings file
-# Ignore the timestamp column
-ratings = pd.read_csv('ratings.csv', sep='\t', encoding='latin-1', usecols=['user_id', 'movie_id', 'rating'])
+import seaborn as sb
+import wordcloud
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-# Reading users file
-users = pd.read_csv('users.csv', sep='\t', encoding='latin-1', usecols=['user_id', 'gender', 'zipcode', 'age_desc', 'occ_desc'])
+# Reading the datasets using pandas
+ratings_ds = pd.read_csv('ratings.csv', sep='\t', encoding='latin-1', usecols=['user_id', 'movie_id', 'rating'])
+users_ds = pd.read_csv('users.csv', sep='\t', encoding='latin-1', usecols=['user_id', 'gender', 'zipcode', 'age_desc', 'occ_desc'])
+movies_ds = pd.read_csv('movies.csv', sep='\t', encoding='latin-1', usecols=['movie_id', 'title', 'genres'])
+ds = pd.merge(pd.merge(movies_ds, ratings_ds), users_ds)
 
-# Reading movies file
-movies = pd.read_csv('movies.csv', sep='\t', encoding='latin-1', usecols=['movie_id', 'title', 'genres'])
-# Import new libraries
+#Describing and visualizing the ratings dataset
+sb.set_style('whitegrid')
+sb.set(font_scale=1.5)
+sb.distplot(ratings_ds['rating'].fillna(ratings_ds['rating'].median()))
+ratings_ds['rating'].describe()
+# Retrieve movies with highest ratings
+ds[['title', 'genres', 'rating']].sort_values('rating', ascending=True).head(100)
 
+# Creating a wordcloud visualization containing certain words that feature more often in Movie Titles
+movies_ds['title'] = movies_ds['title'].fillna("").astype('str')
+docTitle = ' '.join(movies_ds['title'])
+wordcloudTitle = WordCloud(stopwords=STOPWORDS, background_color='black', height=1000, width=2000).generate(docTitle)
+plot.figure(figsize=(16, 8))
+plot.imshow(wordcloudTitle)
+plot.axis('off')
+plot.show()
 
-# Create a wordcloud of the movie titles
-movies['title'] = movies['title'].fillna("").astype('str')
-title_corpus = ' '.join(movies['title'])
-title_wordcloud = WordCloud(stopwords=STOPWORDS, background_color='black', height=2000, width=4000).generate(title_corpus)
-
-# Plot the wordcloud
-plt.figure(figsize=(16,8))
-plt.imshow(title_wordcloud)
-plt.axis('off')
-plt.show()
-
-ratings['rating'].describe()
-
-import seaborn as sns
-sns.set_style('whitegrid')
-sns.set(font_scale=1.5)
-#%matplotlib inline
-
-# Display distribution of rating
-sns.distplot(ratings['rating'].fillna(ratings['rating'].median()))
-
-# Join all 3 files into one dataframe
-dataset = pd.merge(pd.merge(movies, ratings),users)
-# Display 20 movies with highest ratings
-dataset[['title','genres','rating']].sort_values('rating', ascending=True).head(100)
-
-# Make a census of the genre keywords
-genre_labels = set()
-for s in movies['genres'].str.split('|').values:
-    genre_labels = genre_labels.union(set(s))
+# Create a count of genres
+genreNames = set()
+for name in movies_ds['genres'].str.split('|').values:
+    genreNames = genreNames.union(set(name))
 
 # Function that counts the number of times each of the genre keywords appear
-def count_word(dataset, ref_col, census):
-    keyword_count = dict()
-    for s in census: 
-        keyword_count[s] = 0
-    for census_keywords in dataset[ref_col].str.split('|'):        
-        if type(census_keywords) == float and pd.isnull(census_keywords): 
-            continue        
-        for s in [s for s in census_keywords if s in census]: 
-            if pd.notnull(s): 
-                keyword_count[s] += 1
+def wordCounter(ds, ref_col, count):
+    keyCount = dict()
+    for s in count:
+        keyCount[s] = 0
+    for keywordCount in ds[ref_col].str.split('|'):
+        if type(keywordCount) == float and pd.isnull(keywordCount):
+            continue
+        for s in [s for s in keywordCount if s in count]:
+            if pd.notnull(s):
+                keyCount[s] += 1
     #______________________________________________________________________
     # convert the dictionary in a list to sort the keywords by frequency
-    keyword_occurences = []
-    for k,v in keyword_count.items():
-        keyword_occurences.append([k,v])
-    keyword_occurences.sort(key = lambda x:x[1], reverse = True)
-    return keyword_occurences, keyword_count
+    occurence = []
+    for k,v in keyCount.items():
+        occurence.append([k,v])
+    occurence.sort(key = lambda x:x[1], reverse = True)
+    return occurence, keyCount
 
-# Calling this function gives access to a list of genre keywords which are sorted by decreasing frequency
-keyword_occurences, dum = count_word(movies, 'genres', genre_labels)
-#keyword_occurences
+occurence, dum = wordCounter(movies_ds, 'genres', genreNames)
 
-# Define the dictionary used to produce the genre wordcloud
+# Create the dictionary to produce a wordcloud of the movie genres
 genres = dict()
-trunc_occurences = keyword_occurences[0:18]
-for s in trunc_occurences:
-    genres[s[0]] = s[1]
+trunc_occurences = occurence[0:18]
+for name in trunc_occurences:
+    genres[name[0]] = name[1]
 
-# Create the wordcloud
+# Create and display the wordcloud
 genre_wordcloud = WordCloud(width=1000,height=400, background_color='white')
 genre_wordcloud.generate_from_frequencies(genres)
-
-# Plot the wordcloud
-f, ax = plt.subplots(figsize=(16, 8))
-plt.imshow(genre_wordcloud, interpolation="bilinear")
-plt.axis('off')
-plt.show()
+f, ax = plot.subplots(figsize=(16, 8))
+plot.imshow(genre_wordcloud, interpolation="bilinear")
+plot.axis('off')
+plot.show()
 
 # Break up the big genre string into a string array
-movies['genres'] = movies['genres'].str.split('|')
+movies_ds['genres'] = movies_ds['genres'].str.split('|')
 # Convert genres to string value
-movies['genres'] = movies['genres'].fillna("").astype('str')
+movies_ds['genres'] = movies_ds['genres'].fillna("").astype('str')
 
-from sklearn.feature_extraction.text import TfidfVectorizer
 tf = TfidfVectorizer(analyzer='word',ngram_range=(1, 2),min_df=0, stop_words='english')
-tfidf_matrix = tf.fit_transform(movies['genres'])
-#tfidf_matrix.shape
+tfidf_matrix = tf.fit_transform(movies_ds['genres'])
 
 from sklearn.metrics.pairwise import linear_kernel
 cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
-#cosine_sim
 
-# Build a 1-dimensional array with movie titles
-titles = movies['title']
-indices = pd.Series(movies.index, index=movies['title'])
+# Build an array with movie titles
+movie_titles = movies_ds['title']
+indices = pd.Series(movies_ds.index, index=movies_ds['title'])
 
-# Function that get movie recommendations based on the cosine similarity score of movie genres
+# Function to retrieve movies based on the cosine similarity score of movie genres
 def genre_recommendations(title):
     if(title is not None):
         idx = indices[title]
-        sim_scores = list(enumerate(cosine_sim[idx]))
-        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-        print(sim_scores)
-        sim_scores = sim_scores[1:21]
-        print(len(sim_scores))
-        movie_indices = [i[0] for i in sim_scores]
+        similarityScore = list(enumerate(cosine_sim[idx]))
+        similarityScore = sorted(similarityScore, key=lambda x: x[1], reverse=True)
+        print(similarityScore)
+        similarityScore = similarityScore[1:21]
+        print(len(similarityScore))
+        movie_indices = [i[0] for i in similarityScore]
         print(movie_indices)
-        return titles.iloc[movie_indices]
+        return movie_titles.iloc[movie_indices]
     else:
         print('here',title)
-        return indices.head(20) 
+        return indices.head(20)
 
-#genre_recommendations(None)
-#genre_recommendations('Good Will Hunting (1997)').head(20)
+# Fill NaN values in user_id and movie_id column with 0 and in rating column with average of all values
+ratings_ds['user_id'] = ratings_ds['user_id'].fillna(0)
+ratings_ds['movie_id'] = ratings_ds['movie_id'].fillna(0)
+ratings_ds['rating'] = ratings_ds['rating'].fillna(ratings_ds['rating'].mean())
 
-# Fill NaN values in user_id and movie_id column with 0
-ratings['user_id'] = ratings['user_id'].fillna(0)
-ratings['movie_id'] = ratings['movie_id'].fillna(0)
-
-# Replace NaN values in rating column with average of all values
-ratings['rating'] = ratings['rating'].fillna(ratings['rating'].mean())
-
-# Randomly sample 1% of the ratings dataset
-small_data = ratings.sample(frac=0.02)
-# Check the sample info
-#print(small_data.info())
+# Randomly sample of the ratings dataset
+sampleDS = ratings_ds.sample(frac=0.02)
 
 from sklearn import cross_validation as cv
-train_data, test_data = cv.train_test_split(small_data, test_size=0.2)
+train_data, test_data = cv.train_test_split(sampleDS, test_size=0.2)
 
 # Create two user-item matrices, one for training and another for testing
-train_data_matrix = train_data.as_matrix(columns = ['user_id', 'movie_id', 'rating'])
-test_data_matrix = test_data.as_matrix(columns = ['user_id', 'movie_id', 'rating'])
-
-# Check their shape
-#print(train_data_matrix.shape)
-#print(test_data_matrix.shape)
+training_matrix = train_data.as_matrix(columns = ['user_id', 'movie_id', 'rating'])
+test_matrix = test_data.as_matrix(columns = ['user_id', 'movie_id', 'rating'])
 
 from sklearn.metrics.pairwise import pairwise_distances
 
-# User Similarity Matrix
-user_correlation = 1 - pairwise_distances(train_data, metric='correlation')
-user_correlation[np.isnan(user_correlation)] = 0
-#print(user_correlation[:4, :4])
+# Create User Similarity Matrix
+user_pcorr = 1 - pairwise_distances(train_data, metric='correlation')
+user_pcorr[np.isnan(user_pcorr)] = 0
 
-# Item Similarity Matrix
-item_correlation = 1 - pairwise_distances(train_data_matrix.T, metric='correlation')
-item_correlation[np.isnan(item_correlation)] = 0
-#print(item_correlation[:4, :4])
+# Create Item Similarity Matrix
+item_pcorr = 1 - pairwise_distances(training_matrix.T, metric='correlation')
+item_pcorr[np.isnan(item_pcorr)] = 0
 
 # Function to predict ratings
 def predict(ratings, similarity, type='user'):
@@ -176,24 +140,24 @@ def predict(ratings, similarity, type='user'):
         pred = ratings.dot(similarity) / np.array([np.abs(similarity).sum(axis=1)])
     return pred
 
-from sklearn.metrics import mean_squared_error
-from math import sqrt
-
 # Function to calculate RMSE
-def rmse(pred, actual):
-    # Ignore nonzero terms.
+def calculateRMSE(pred, actual):
     pred = pred[actual.nonzero()].flatten()
     actual = actual[actual.nonzero()].flatten()
-    return sqrt(mean_squared_error(pred, actual))
+    rmse = sqrt(mean_squared_error(pred, actual))
+    return rmse
 
 # Predict ratings on the training data with both similarity score
-user_prediction = predict(train_data_matrix, user_correlation, type='user')
-item_prediction = predict(train_data_matrix, item_correlation, type='item')
+user_based_pred = predict(training_matrix, user_pcorr, type='user')
+item_based_pred = predict(training_matrix, item_pcorr, type='item')
 
-# RMSE on the test data
-print('User-based CF RMSE: ' + str(rmse(user_prediction, test_data_matrix)))
-print('Item-based CF RMSE: ' + str(rmse(item_prediction, test_data_matrix)))
-
-# RMSE on the train data
-print('User-based CF RMSE: ' + str(rmse(user_prediction, train_data_matrix)))
-print('Item-based CF RMSE: ' + str(rmse(item_prediction, train_data_matrix)))
+# Root Mean Square Error on the train data
+print('User-based Collaborating Filtering RMSE:')
+print(str(calculateRMSE(user_based_pred, training_matrix)))
+print('Item-based Collaborating Filtering RMSE:')
+print(str(calculateRMSE(item_based_pred, training_matrix)))
+# Root Mean Square Error on the test data
+print('User-based Collaborating Filtering RMSE:')
+print(str(calculateRMSE(user_based_pred, test_matrix)))
+print('Item-based Collaborating Filtering RMSE:')
+print(str(calculateRMSE(item_based_pred, test_matrix)))
